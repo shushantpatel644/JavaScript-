3f79116790b17cc9877de48b1485834e1924ed85
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-01-22 23:39:46 +0530

    JavaScript Developer

---
+++ 13_strings.js
@@ -0,0 +1,27 @@
+// string is a collection of charchaters
+
+let name = "Shushant"
+console.log(name.length)
+console.log(name[0])
+console.log(name[1])
+
+// let friend = 'shushanttt" // don't do this
+// console.log(friend)
+
+
+//Templates literals  templates literals use insted of quotes to define a string
+
+let boy1 = "Shushant"
+let boy2 = "Himanshu"
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+//string Interplation -: we can insert variable directly in templates literal this is called string interplation 
+
+//Escape sequence characters
+let fruit = 'bana\'na'
+console.log(fruit)
+let adm = 'Adam D\'Angelo'
+console.log(adm)
+
+

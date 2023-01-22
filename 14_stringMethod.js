8f4f576b84ec9546b6fd6f07177373cb647e38f2
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-01-22 23:58:07 +0530

    JavaScript Developer

---
+++ 14_stringMethod.js
@@ -0,0 +1,23 @@
+// let name = "Shushant"
+// console.log(name.length)
+// console.log(name.toUpperCase())
+// console.log(name.toLowerCase())
+// console.log(name.slice(2, 4))
+// console.log(name.slice(2))
+// console.log(name.replace("Shu", "Pat"))
+
+// let friend = 'Mohhit'
+// console.log((name.concat(" is a friend of ", friend, " ok")))
+// let fir2 = "         miss"
+// console.log(fir2)
+// console.log(fir2.trim())
+
+let fr = "Shivika"
+let fr1 = "Shivika" + "Shivika" + "harry"
+
+console.log(fr[0])
+console.log(fr[2])
+console.log(fr[3])
+console.log(fr[4])
+fr[4] = "o" //this is not possible
+console.log(fr[5])

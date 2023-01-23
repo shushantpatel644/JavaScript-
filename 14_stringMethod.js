7826fc764f305165dbc614d1a84e1141fac86919
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-01-23 22:16:27 +0530

    JavaScript Developer

--- 14_stringMethod.js
+++ 14_stringMethod.js
@@ -12,12 +12,21 @@
 // console.log(fir2)
 // console.log(fir2.trim())
 
-let fr = "Shivika"
-let fr1 = "Shivika" + "Shivika" + "harry"
+// let fr = "Shivika"
+// let fr1 = "Shivika" + "Shivika" + "harry"
 
-console.log(fr[0])
-console.log(fr[2])
-console.log(fr[3])
-console.log(fr[4])
-fr[4] = "o" //this is not possible
-console.log(fr[5])
+// console.log(fr[0])
+// console.log(fr[2])
+// console.log(fr[3])
+// console.log(fr[4])
+// fr[4] = "o" //this is not possible
+// console.log(fr[5])
+
+// string are immutable inorder to acces the character at an index we use the following syntax
+
+
+let friend = "Shushant"
+let n = input("Enter the string")
+for (let i = 0; i < n; i++) {
+    friend[i];
+}

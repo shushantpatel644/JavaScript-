a21e462d866bb89c30e0efaccbe37208e0b7c582
Shushant Patel <97536200+shushantpatel644@users.noreply.github.com>
2023-04-26 14:11:21 +0530

    JavaScript

---
+++ Arrys IN JavaScript/index.js
@@ -0,0 +1,49 @@
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// let arr2 = ["Shushant", "patel"];
+// let arr3 = [1, 2, 3, 4, 5, 6, 7];
+// // console.log(arr);
+// // console.log(arr2[0]);
+// // console.log(arr2[1]);
+// // console.log(arr3);
+
+
+
+// let arr = [1, 2, 3];
+// // arr.push(9);
+// // arr.pop();
+// arr.shift(); //remove element from first
+// arr.unshift(); //add the element from the frist index of the array
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9];
+// arr.splice(4, 3);
+// console.log(arr);
+
+
+// let arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9];
+// arr.reverse();
+// arr[0] = [190];
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5, 5, 6, 7, 8, 9];
+// let newArr = arr.slice(1, 5);
+// console.log(arr);
+// console.log(newArr);
+
+// let arr =[];
+// for(let i=0;i<10;i++){
+//     arr.push("Shushi");
+// }
+// console.log(arr);
+
+
+// ***************************Objects******************
+
+const myObj = {
+    name: "Sbhsuahnt",
+    id: 190011,
+    add: "banda",
+    income: 64738920,
+    male: true,
+};
+console.log(myObj); 

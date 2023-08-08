5bd1596dcb3280da967c59fc4a6956f848b1ecc2
shushantpatel644 <patelshushant695@gmail.com>
2023-08-09 00:44:17 +0530

    8-8-23

---
+++ JAVASCRIPT BY 6 PACK PROGRAMMER/opps.js
@@ -0,0 +1,17 @@
+// const a ="Hello world";
+// reference data types
+// const b={
+//     name:"Shushat",
+//     printSunernam:()=>{
+//         console.log("Singh");
+//     },
+// }
+// console.log(a);
+
+
+// console.log(b);
+// b.added= "value is added";
+// console.log(b);
+
+
+ 

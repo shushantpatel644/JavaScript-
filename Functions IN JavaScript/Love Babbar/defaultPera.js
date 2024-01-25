bd3b59adcab52ab605515310114e2da58b69cda6
shushantpatel644 <patelshushant695@gmail.com>
2024-01-25 23:26:45 +0530

    25 Jan

---
+++ Functions IN JavaScript/Love Babbar/defaultPera.js
@@ -0,0 +1,30 @@
+// default parametre
+
+// function interest(p,r=7,y){
+//     return p*r*y/100;
+// }
+// // console.log(interest(1000,8));
+// console.log(interest(1000,undefined,8));
+
+
+
+
+let person = {
+    fName :"Shushant",
+    lName :"Patel"
+};
+function fullName(){
+    return `${person.fName} ${person.lName}`
+}
+console.log(fullName());
+
+
+// getter -> excess the properties 
+
+// shetter -> chnage and update the property (or mutate properties)
+
+
+// try 
+// catch
+ 
+
--- Functions IN JavaScript/Love Babbar/main.js
+++ Functions IN JavaScript/Love Babbar/main.js
@@ -1,28 +1,54 @@
 //What:-  funtion is a block of code that fulfil a specific task
 // Why :- reusability .. bulkey code se bach jate ha
-run();
+// run();
 
-function run(){
-console.log("running");
-}
+// function run(){
+// console.log("running");
+// }
 
-// call funtion or invoke karna  
-// hosting is the process of moving funtion declaration to the top of file. and this is an automatically
+// // call funtion or invoke karna  
+// // hosting is the process of moving funtion declaration to the top of file. and this is an automatically
 
-// Named funtion assignment
-let stand = function walk(){
-    console.log('walking');
-};
-stand();
+// // Named funtion assignment
+// let stand = function walk(){
+//     console.log('walking');
+// };
+// stand();
 
-// Anonymous funtion assignment
-let stand2 = function(){
-    console.log('walking two');
-};
-stand2();
+// // Anonymous funtion assignment
+// let stand2 = function(){
+//     console.log('walking two');
+// };
+// stand2();
 
 
 // here hoting is not allowed
-let jump = stand ;
-jump();
+// let jump = stand ;
+// jump();
 
+
+
+// let a=1;
+// a='x';
+// console.log(a);
+
+
+// function sum(a,b){
+//     let total = 0;
+//     for(let value of arguments)
+//         total = total + value;
+//     // console.log(arguments);
+//     return total;
+// }
+
+// // it is an object type not a array 
+
+// // console.log(sum(2,3))
+// console.log(sum(1,2,3,4,5,6)) //NaN
+
+//rest operator
+
+// function sum(num,value , ...args){
+//     console.log(args);
+// }
+// sum(1,2,3,4,5,6,7,7,8,9,0);

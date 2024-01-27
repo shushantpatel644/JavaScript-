711f978437b02b2eb0d546170cf2788d57309e0b
shushantpatel644 <patelshushant695@gmail.com>
2024-01-27 22:35:23 +0530

    27 Jan

--- Functions IN JavaScript/Love Babbar/defaultPera.js
+++ Functions IN JavaScript/Love Babbar/defaultPera.js
@@ -8,6 +8,24 @@
 
 
 
+let person = {
+    fName :"Shushant",
+    lName :"Patel"
+};
+function fullName(){
+    return `${person.fName} ${person.lName}`
+}
+console.log(fullName());
+// default parametre
+
+// function interest(p,r=7,y){
+//     return p*r*y/100;
+// }
+// // console.log(interest(1000,8));
+// console.log(interest(1000,undefined,8));
+
+
+
 
 let person = {
     fName :"Shushant",

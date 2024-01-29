813a16b43d1cf1ab122b96bb763b5a073e70d423
shushantpatel644 <patelshushant695@gmail.com>
2024-01-29 16:28:57 +0530

    Jan 29

--- Js Dom Love Babbar/index.js
+++ Js Dom Love Babbar/index.js
@@ -7,12 +7,29 @@
 
 
 //2nd way
-function eventFuntion(){
-    console.log(' I have clicked on the document');
-}
-document.addEventListener('click',eventFuntion);
+// function eventFuntion(){
+//     console.log(' I have clicked on the document');
+// }
+// document.addEventListener('click',eventFuntion);
 
 
 // remove eventListner
 
- 
+
+// const content = document.querySelector('#wrapper');
+
+// content.addEventListener('click',function(event){
+//     console.log(event);
+// })
+
+//the default action // prevent default
+
+let links = document.querySelectorAll('a');
+let thirdLink = links[2];
+
+thirdLink.addEventListener('click',function(event){
+    event.preventDefault();
+    console.log('maza aaya, accha laga');
+});
+
+
